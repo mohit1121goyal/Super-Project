ea9034f8eada585ab5f05bfd3462dac1af0cd6cb
mohit1121goyal <mohit1121goyal@gmail.com>
2025-12-05 14:32:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,23 +11,27 @@ provider "aws" {
   region = var.aws_region  # Change to your desired region
 }
 
+
 module "nexus_instance" {
-  source       = "./ec2_instance"
-  ami          = var.aws_ami  # Example AMI ID, replace with your desired AMI
-  instance_type = var.aws_type             # Example instance type, replace with your desired type
+  source        = "./ec2_instance"
+  ami           = var.aws_ami
+  instance_type = var.aws_type
   instance_name = "nexus"
+  key_name      = var.aws_key_name
 }
 
 module "sonar_instance" {
-  source       = "./ec2_instance"
-  ami          = var.aws_ami  # Example AMI ID, replace with your desired AMI
-  instance_type = var.aws_type              # Example instance type, replace with your desired type
+  source        = "./ec2_instance"
+  ami           = var.aws_ami
+  instance_type = var.aws_type
   instance_name = "sonar"
+  key_name      = var.aws_key_name
 }
 
 module "test_instance" {
-  source       = "./ec2_instance"
-  ami          = var.aws_ami  # Example AMI ID, replace with your desired AMI
-  instance_type = var.aws_type              # Example instance type, replace with your desired type
+  source        = "./ec2_instance"
+  ami           = var.aws_ami
+  instance_type = var.aws_type
   instance_name = "test"
+  key_name      = var.aws_key_name
 }
